471189df21b6acc2d07f5b9ab082acd49f257696
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-20 15:27:05 +0100

    .

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,15 +2,15 @@ import scala.annotation.tailrec
 
 @main
 def main(): Unit =
-   collatz(27)
+   val res = collatz(5923).reverse
+   println(res)
+   println(s"Steps: ${res.length - 1}")
+   println
 
 @tailrec
-def collatz(n: Long, i: Int = 0): Unit =
-   if n <= 1 then
-      println(n)
-      println(s"Steps: $i")
+def collatz(n: Long, acc: List[Long] = Nil): List[Long] =
+   if n <= 1 then n :: acc
    else
-      print(s"$n, ")
       n % 2 match
-         case 0 => collatz(n / 2, i + 1)
-         case _ => collatz(3 * n + 1, i + 1)
+         case 0 => collatz(n / 2, n :: acc)
+         case _ => collatz(3 * n + 1, n :: acc)
