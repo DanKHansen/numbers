131d8f31ee3eba0aa615c88ce38a255568049c39
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-01-21 15:37:19 +0100

    playing with Collatz and primes

--- src/main/scala/numbers.scala
+++ src/main/scala/numbers.scala
@@ -5,10 +5,10 @@ def main(): Unit =
    val result = for n <- primes.takeWhile(_ <= 2000).toList yield (n, collatz(n).reverse)
 
    result
-      //.filter(_._2.head.toString.endsWith("17"))
-      .sortBy(_._2.length-1)
-      //.foreach(r => println(s"${r._2}"))
-      //.foreach(r => println(s"Step: ${r._2.length - 1}"))
+      // .filter(_._2.head.toString.endsWith("17"))
+      .sortBy(_._2.length - 1)
+      // .foreach(r => println(s"${r._2}"))
+      // .foreach(r => println(s"Step: ${r._2.length - 1}"))
       .foreach(r => println(s"Step: ${r._2.length - 1} \npath: ${r._2.mkString(",")}\n"))
 
 @tailrec
@@ -19,7 +19,8 @@ def collatz(n: Long, acc: List[Long] = Nil): List[Long] =
          case 0 => collatz(n / 2, n :: acc)
          case _ => collatz(3 * n + 1, n :: acc)
 
-val primes: LazyList[Int] = 2 #:: LazyList.from(3,2).filter(isPrime)
-   def isPrime(n: Int): Boolean =
-      primes.takeWhile(p => p*p <= n).forall(n % _ != 0)
+val primes: LazyList[Int] =
+   2 #:: LazyList.from(3, 2).filter(isPrime)
 
+def isPrime(n: Int): Boolean =
+   primes.takeWhile(p => p * p <= n).forall(n % _ != 0)
